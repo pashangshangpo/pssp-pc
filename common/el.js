8b4b81c04ce2d9bc1421837c2fb81278fe0d90cf
爬上上坡 <pashangshangpo@gmail.com>
2018-04-05 10:20:10 +0800

    添加el工具

---
+++ common/el.js
@@ -0,0 +1,18 @@
+/**
+ * @file 创建元素工具
+ * @author pashangshangpo
+ */
+
+import React from 'react'
+
+/**
+ * 快捷创建元素
+ * 
+ * @def: el: element, prop, children => Element
+ *  element: [String, ReactElement]
+ *  prop: Object
+ *  children: [String, ReactElement]
+ */
+export default (element, prop, children) => {
+  return React.createElement(element, prop, children)
+}
