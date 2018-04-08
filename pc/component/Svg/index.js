595fd6f34f7e91e2872b9739c4135d1c34895ada
pashangshangpo <pashangshangpo@gmail.com>
2018-04-08 19:21:38 +0800

    添加aria-hidden

--- pc/component/Svg/index.js
+++ pc/component/Svg/index.js
@@ -30,6 +30,7 @@ export default class extends Component {
     return el(
       'svg',
       {
+        'aria-hidden': true,
         className: c({
           default: this.props.className,
           prefix: 'svg'
