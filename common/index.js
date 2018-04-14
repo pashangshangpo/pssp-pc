27359950a92c6b50d463f2ce28b4c41408a487a0
爬上上坡 <pashangshangpo@gmail.com>
2018-04-14 13:13:02 +0800

    修改导出名称

--- common/index.js
+++ common/index.js
@@ -5,5 +5,5 @@
 
 export {default as c} from './c'
 export {default as el} from './el'
-export {default as log} from './log'
+export {default as Log} from './log'
 export {default as treen} from './treen'
