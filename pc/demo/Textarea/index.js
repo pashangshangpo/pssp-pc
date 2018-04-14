ab0decad6ed4f29904249dfffca4d0fefc602460
爬上上坡 <pashangshangpo@gmail.com>
2018-04-14 10:05:57 +0800

    添加Textarea例子

--- pc/demo/Textarea/index.js
+++ pc/demo/Textarea/index.js
@@ -8,14 +8,31 @@ import Textarea from '../../component/Textarea'
 
 export default () => {
   return el(
-    Textarea,
-    {
-      autoFocus: false,
-      placeholder: '自适应高度',
-      value: '',
-      onInput: e => {
-        console.log('Textarea', e.target.value)
+    'div',
+    {},
+    el(
+      Textarea,
+      {
+        autoFocus: false,
+        placeholder: '自适应高度',
+        value: '',
+        onInput: e => {
+          console.log('Textarea', e.target.value)
+        }
       }
-    }
+    ),
+    el(
+      Textarea,
+      {
+        autoFocus: false,
+        placeholder: '限定最小和最大高度',
+        minLine: 3,
+        maxLine: 5,
+        value: '限定最小和最大高度限定最小和最大高度限定最小和最大高度限定最小和最大高度限定最小和最大高度限定最小和最大高度',
+        onInput: e => {
+          console.log('Textarea', e.target.value)
+        }
+      }
+    )
   )
 }
