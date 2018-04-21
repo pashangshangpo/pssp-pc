68e77366b14b402387c1bafe945eb136b0e96a0c
爬上上坡 <pashangshangpo@gmail.com>
2018-04-21 08:59:52 +0800

    修改Button测试用例

--- __tests__/button.test.js
+++ __tests__/button.test.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Button } from '../index'
-import { shallow, mount } from 'enzyme'
-import el from '../../common/el'
-import '../../enzyme.adapter'
+/**
+ * @file Button测试用例
+ * @author pashangshangpo
+ */
 
-describe('Button', () => {
-    it('Button测试', () => {
+import React from 'react'
+import Button from '../component/Button'
+import { shallow, mount } from 'enzyme'
+import el from '../common/el'
+import '../enzyme.adapter'
+
+describe('Button测试用例', () => {
+    it('类名是否正确', () => {
         let wrapper = shallow(el(
             Button,
             {
