8e9ac051c28e036cef583abd45ef765a8fb74ff4
爬上上坡 <pashangshangpo@gmail.com>
2018-04-22 14:26:05 +0800

    修改demo

--- demo/Layout/index.js
+++ demo/Layout/index.js
@@ -29,14 +29,14 @@ export default () => {
           checked: 'nav 1',
           data: [
             {
-              name: 'nav 1',
+              name: 'nav 1'
             },
             {
-              name: 'nav 2',
-              children: ['nav2 1', 'nav2 2']
+              name: 'nav 2'
             },
             {
               name: 'nav 3',
+              children: ['nav3 1', 'nav3 2']
             }
           ]
         }
