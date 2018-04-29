e44c35bdda91a3f3dbcec304f849f2b1502594b6
爬上上坡 <pashangshangpo@gmail.com>
2018-04-29 16:10:57 +0800

    init Hotkey

---
+++ component/Hotkey/index.js
@@ -0,0 +1,21 @@
+/**
+ * @file Hotkey
+ * @author pashangshangpo
+ * @createTime 2018年4月29日 下午4:09
+ */
+
+import React, { Component } from 'react'
+import { el } from '../../common'
+
+export default class extends Component {
+  /**
+   * @def-start: Hotkey: props => Hotkey
+   *  props: Object
+   */
+  static defaultProps = {
+  }
+
+  render() {
+    return null
+  }
+}
