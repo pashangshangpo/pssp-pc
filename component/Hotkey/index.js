661ab345e8b50b1e03103d37cfbfff24126e92b0
爬上上坡 <pashangshangpo@gmail.com>
2018-04-29 17:39:33 +0800

    组合事件

--- component/Hotkey/index.js
+++ component/Hotkey/index.js
@@ -28,8 +28,40 @@ export default class extends Component {
   handleKeyDown = e => {
     // 如果没有children则当做全局事件来处理
     if (e.target === this.dom || this.props.children.length === 0) {
-      
+      console.log(this.combination(e))
     }
+  }
+
+  combination = e => {
+    const keys = [
+      {
+        name: 'meta',
+        key: 'metaKey',
+        keyCode: 91
+      },
+      {
+        name: 'ctrl',
+        key: 'ctrlKey',
+        keyCode: 17
+      },
+      {
+        name: 'alt',
+        key: 'altKey',
+        keyCode: 18
+      },
+      {
+        name: 'shift',
+        key: 'shiftKey',
+        keyCode: 16
+      }]
+
+    const current = keys.find(item => e[item.key] && e.keyCode !== item.keyCode)
+    
+    if (current) {
+      return `${current.name}+${e.keyCode}`
+    }
+    
+    return e.keyCode
   }
 
   render() {
